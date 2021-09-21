2e03946a2cdc1172eefeb0ef6da7727bdb976a33
Amie Foster <amie.n.foster@gmail.com>
2021-09-21 11:09:37 -0600

    links not passing but working properly in browser

--- src/components/links.js
+++ src/components/links.js
@@ -3,9 +3,10 @@ import React from "react";
 function Links(props) {
   return (
     <>
-      <h3>Links</h3>
-      <a href={props.links.github}>{props.links.github}</a>
-      <a href={props.links.linkedin}>{props.links.linkedin}</a>
+      <h3>Links </h3>
+      <a href={props.link.github}>{props.link.github}</a>
+      {/* <a href={props.links.github}>{props.links.github}</a> */}
+      <a href={props.link.linkedin}>{props.link.linkedin}</a>
     </>
   );
 }
